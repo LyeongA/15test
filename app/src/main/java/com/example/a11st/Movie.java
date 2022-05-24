e6aebd6dde3b8e11cef0171c6a795abccecfb47c
LyeongA <iyeong617@gmail.com>
2022-05-24 16:15:12 +0900

    영화 정보 추가

---
+++ app/src/main/java/com/example/a11st/Movie.java
@@ -0,0 +1,20 @@
+package com.example.a11st;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndNew;
+    String movieCd;
+    String movieNm;
+    String openDt;
+    String salesAmt;
+    String salesShare;
+    String salesInten;
+    String salesAcc;
+    String audiCnt;
+    String audiChange;
+    String audiAcc;
+    String scrnCnt;
+    String showCht;
+}
